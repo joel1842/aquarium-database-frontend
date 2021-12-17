75a6e9a6bc01be30172ec6a182392bdce4a1b532
joel1842 <joeldaniellake@gmail.com>
2021-12-17 13:45:31 -0330

    Added multiple tank shapes to calculator

--- src/routes/TankSize/TankSizeCalculator.js
+++ src/routes/TankSize/TankSizeCalculator.js
@@ -1,10 +1,11 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import './tanksize.css';
 
 const TankSizeCalculator = () => {
 
     const [length, setLength] = useState();
     const [width, setWidth] = useState();
+    const [fullWidth, setFullWidth] = useState();
     const [depth, setDepth] = useState();
     const [tankVolume, setTankVolume] = useState();
 
@@ -12,15 +13,84 @@ const TankSizeCalculator = () => {
     const [unit, setUnit] = useState('Cm');
     const [unitString, setUnitString] = useState('Gallons');
     const [outputString, setOutputString] = useState('Liters');
+    
+    const [rectangular, setRectangular] = useState(true);
+    const [cube, setCube] = useState(false);
+    const [bowFront, setBowFront] = useState(false);
+    const [cylinder, setCylinder] = useState(false);
+
+    const [tankType, setTankType] = useState("Rectangular")
+
+    const tankBoolSwitch = () => {
+
+        console.log(tankType)
+        if (tankType === "Rectangular") {
+            setRectangular(true);
+            setCube(false);
+            setBowFront(false);
+            setCylinder(false);
+        } else if (tankType === "Cube") {
+            setRectangular(false);
+            setCube(true);
+            setBowFront(false);
+            setCylinder(false);
+        } else if (tankType === "Bow Front") {
+            setRectangular(false);
+            setCube(false);
+            setBowFront(true);
+            setCylinder(false);
+        } else if (tankType === "Cylinder") {
+            setRectangular(false);
+            setCube(false);
+            setBowFront(false);
+            setCylinder(true);
+        }
+    }
+
+    const tankSwitch = (event) => {
+        setTankType(event.target.value);
+    }
+
+    useEffect(() => {
+        tankBoolSwitch();
+    }, [tankType])
 
     const getVolume = () => {
 
-        const volume = length * width * depth
-        
+        let volume;
+
+        const getRectangularVolume = () => {
+            volume = length * width * depth;
+        }
+
+        const getCubeVolume = () => {
+            volume = length * length * length;
+        }
+
+        const getBowFrontVolume = () => {
+            let squareArea = length * width;
+            let elipticalArea = Math.PI * (length / 2) * (fullWidth - width) / 2;
+            volume = (squareArea + elipticalArea) * depth;
+        }
+
+        const getCylinderVolume = () => {
+            let radius = width / 2;
+            volume = Math.PI * (radius * radius) * depth;
+        }
+
+        if (cylinder) {
+            getCylinderVolume()
+        } else if (cube) {
+            getCubeVolume()
+        } else if (bowFront) {
+            getBowFrontVolume()
+        } else {
+            getRectangularVolume()
+        }
+
         if (unitBool === true) {
             // inch
             setTankVolume(volume / 1000);
-            ;
         } else {
             // cm
             setTankVolume(volume / 231);
@@ -54,24 +124,43 @@ const TankSizeCalculator = () => {
                     <h2 className='subHeader'>Calculate Tank Size in {outputString}</h2>
                     <button onClick={unitSwitch}>Use {unitString}</button>
                 </div>
+                <div className="selectContainer">
+                    <label className="selectHeader">Choose tank shape:</label>
+                    <select className="tankShapes" name="tankShapes" onChange={tankSwitch}>
+                        <option value="Rectangular">Rectangular</option>
+                        <option value="Cube">Cube</option>
+                        <option value="Bow Front">Bow Front</option>
+                        <option value="Cylinder">Cylinder</option>
+                    </select>
+                </div>
+                {cylinder ? null : 
                 <div>
                     <input className="length" type="number" placeholder="Length" onChange={(event) => {
                         setLength(event.target.value);
                     }}></input>
                     <h2 className='unit'>{unit}</h2>
-                </div>
+                </div>}
+                {cube ? null :
                 <div>
                     <input className="height" type="number" placeholder="Width" onChange={(event) => {
                         setWidth(event.target.value);
                     }}></input>
                     <h2 className='unit'>{unit}</h2>
-                </div>
+                </div>}
+                {cube ? null : 
                 <div>
                     <input className="depth" type="number" placeholder="Depth" onChange={(event) => {
                         setDepth(event.target.value);
                     }}></input>
                     <h2 className='unit'>{unit}</h2>
-                </div>
+                </div>}
+                {(rectangular || cube || cylinder) ? null : 
+                <div>
+                    <input className="fullWidth" type="number" placeholder="Full Width" onChange={(event) => {
+                        setFullWidth(event.target.value);
+                    }}></input>
+                    <h2 className='unit'>{unit}</h2>
+                </div>}
                 <div className="outputContainer">
                     <button onClick={getVolume}>Calculate Volume!</button>
                     {tankVolume && 
@@ -80,7 +169,6 @@ const TankSizeCalculator = () => {
                     </div>}
                 </div>
             </div>
-
         </div>
     )
 }
